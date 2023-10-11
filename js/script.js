1296b0b44b99973a9cc121b88ae7787daae139b3
AdrianKnudsen <adrian@knudsen.no>
2023-10-11 04:02:53 +0200

    added array to js

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const QuizQuestions = [
+    {
+      question: "What is the name of the paper company where most of the action in 'The Office' takes place?",
+      answers: ["Dunder Mifflin", "Scranton Paper Co.", "Wernham Hogg", "Paper Paradise"],
+      correctAnswer: "Dunder Mifflin",
+    },
+    {
+      question: "Who is known for his constant pranks on his deskmate, Dwight Schrute?",
+      answers: ["Oscar Martinez", "Stanley Hudson", "Jim Halpert", "Kevin Malone"],
+      correctAnswer: "Jim Halpert",
+    },
+    {
+      question: "Who is the regional manager of Dunder Mifflin's Scranton branch at the start of the series?",
+      answers: ["Jim Halpert", "Michael Scott", "Pam Beesly", "Ryan Howard"],
+      correctAnswer: "Michael Scott",
+    },
+    {
+      question: "Michael and Dwight tried to steal clients from which local competing business?",
+      answers: ["Paper Mill", "Prince Family Paper", "Great Paper", "Stapels"],
+      correctAnswer: "Prince Family Paper.",
+    },
+    {
+      question: "Finish Dwight's security code: 'The tea in Nepal is very hot...'",
+      answers: ["But the coffee in Peru is much hotter.", "Bears, beets, Battlestar Galactica.", "Schrute Farms, Bed & Breakfast.", "Dunder Mifflin, the paper people."],
+      correctAnswer: "But the coffee in Peru is much hotter.",
+    },
+    {
+      question: "What is the title of Michael's movie?",
+      answers: ["Threat Level Midnight.", "The Dundies", "Scott's Tots", "Paper Jam"],
+      correctAnswer: "Threat Level Midnight."
+    },
+    {
+      question: "What is the name of Dwight's porcupine?",
+      answers: ["Henrietta.", "Spike", "Prickles", "Quilliam"],
+      correctAnswer: "Henrietta."
+    },
+    {
+      question: "Who is known for his obsession with bears, beets, and 'Battlestar Galactica'?",
+      answers: ["Jim Halpert", "Andy Bernard", "Dwight Schrute", "Oscar Martinez"],
+      correctAnswer: "Dwight Schrute",
+    },
+    {
+      question: "Which character's catchphrase is 'That's what she said'?",
+      answers: ["Michael Scott", "Jim Halpert", "Ryan Howard", "Angela Martin"],
+      correctAnswer: "Michael Scott",
+    },
+    {
+      question: "Which of Angela's cats did Dwight kill?",
+      answers: ["Bandit.", "Smokey.", "Fluffy.", "Sprinkles."],
+      correctAnswer: "Sprinkles.",
+    }
+  ];
+  
